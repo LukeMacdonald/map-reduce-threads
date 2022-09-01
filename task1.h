b869c712eee137dddbb6d182439e355b74b24c4b
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2022-09-02 01:24:41 +1000

    Completed Task1

---
+++ task1.h
@@ -0,0 +1,22 @@
+//
+// Created by Luke on 29/8/2022.
+//
+
+
+#ifndef UNTITLED_TASK1_H
+
+#define UNTITLED_TASK1_H
+
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <vector>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <pthread.h>
+void task1filter(const std::string &read_file, const std::string &write_file);
+
+#endif //UNTITLED_TASK1_H
