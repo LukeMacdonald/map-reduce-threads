4b0b68e513eed5506d48c6a9c3afd115f23798ef
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2022-09-02 01:31:36 +1000

    added main menu and makefile

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include "task1.h"
+
+int main(int argc, char *argv[]) {
+    std::string task = argv[1];
+    std::string input_file = argv[2];
+    std::string output_file= argv[3];
+    if (task == "task1"){
+        task1filter(input_file,output_file);
+    }
+  
+  
+    return 0;
+}
