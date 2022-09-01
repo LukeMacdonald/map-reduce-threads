4c551cee783e5d7c357ae2e0c14be8906ddbb332
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2022-09-02 01:29:39 +1000

    updated task1.h

--- task1.h
+++ task1.h
@@ -1,22 +1,13 @@
-//
-// Created by Luke on 29/8/2022.
-//
 
+#ifndef OSPA1_TASK1_H
 
-#ifndef UNTITLED_TASK1_H
-
-#define UNTITLED_TASK1_H
+#define OSPA1_TASK1_H
 
 #include <iostream>
 #include <fstream>
 #include <set>
 #include <vector>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 
-#include <pthread.h>
 void task1filter(const std::string &read_file, const std::string &write_file);
 
-#endif //UNTITLED_TASK1_H
+#endif //OSPA1_TASK1_H
