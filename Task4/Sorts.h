e6ebf568451d0362e64f6eb3b44a9a62a9bcfbf7
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2022-09-04 00:44:00 +1000

    Added Task4

---
+++ Task4/Sorts.h
@@ -0,0 +1,10 @@
+#ifndef SORTS_H
+#define SORTS_H
+
+#include <iostream>
+#include <string>
+
+bool sort_string(std::string s1, std::string s2);
+bool sort_indexes(int i1, int i2);
+
+#endif //SORTS_H
