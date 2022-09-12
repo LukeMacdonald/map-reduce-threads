e7f52696546c56b7baf8c74e92f8b71249031d56
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2022-09-12 15:38:41 +1000

    updated format

--- Task5Server.cpp
+++ Task5Server.cpp
@@ -1,13 +1,9 @@
 #include "Task5Server.h"
 
 int main(int argc, char *argv[]){
-    
     std::string input_file = argv[1];
-    
     Stream* s = new Stream(input_file);
-    
     int size = s->size();
-    
     for(int i = 0; i < size;i++){
         std::cout << s->get() << std::endl;
     }
